ccbf0c0196dbeddb274bef8c96ccd21ab1e621ed
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2020-12-13 15:17:55 +0200

    Videos

--- src/components/utils/VideosPanel/VideosPanel.js
+++ src/components/utils/VideosPanel/VideosPanel.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBRow, MDBCol } from 'mdbreact';
 import ReactPlayer from 'react-player';
 
 import './VideosPanel.scss';
 import Title from '../Title';
 
-function VideosPanel() {
+const VideosPanel = () => {
+  const [showVideo, setShowVideo] = useState(false);
+
   return (
     <MDBRow className="video-panel">
       <MDBCol size="12"><Title text="Videos" /></MDBCol>
-      <MDBCol className="vid-wrapper" size="12">
+      <MDBCol md="6" className="vid-wrapper" size="12">
         <ReactPlayer url={[
-          'https://www.youtube.com/watch?v=UZnTPDy-znE',
-          'https://www.youtube.com/watch?v=bUrGf6Fhi4s',
-        ]}
-        width='50%'
-        height='100%'
-        style={{ marginBottom: '15px' }}
-        controls
-      />
+            'https://www.youtube.com/watch?v=UZnTPDy-znE',
+            'https://www.youtube.com/watch?v=bUrGf6Fhi4s',
+          ]}
+          width='100%'
+          height='100%'
+          style={{ marginBottom: '15px' }}
+          controls
+        />
+      </MDBCol>
+      <MDBCol md="6">
+        <div>
+          <iframe src={`https://www.youtube.com/embed/watch?v=UZnTPDy-znE`}
+            allow="accelerometer;
+            autoplay;
+            encrypted-media;
+            gyroscope;
+            picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
       </MDBCol>
     </MDBRow>
   );
