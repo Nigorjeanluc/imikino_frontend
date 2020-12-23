ac9fd2a1f42091d2c67b0deb7efdb1ce49c47a90
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2020-12-23 07:27:06 +0200

    Fix Share Buttons

--- src/components/utils/HelmetMetaData/index.js
+++ src/components/utils/HelmetMetaData/index.js
@@ -17,14 +17,9 @@ export default function HelmetMetaData(props) {
       <title>{title}</title>
       <meta charset="utf-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="csrf_token" content="" />
       <meta property="type" content="website" />
       <meta property="url" content={currentUrl} />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="_token" content="" />
       <meta name="robots" content="noodp" />
       <meta property="title" content={title} />
       <meta property="quote" content={quote} />
