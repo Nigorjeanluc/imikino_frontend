553fd0f90f1e371e96e3110f9727be42dd8a19cf
Ndiramiye Igor Jean Luc <nigorjeanluc@gmail.com>
2020-12-24 07:25:44 +0200

    Fix share Button

--- src/redux/helpers/backendURLs.js
+++ src/redux/helpers/backendURLs.js
@@ -5,7 +5,7 @@ const SOCKET_URL = 'http://localhost:4005';
 const LOCAL_URL_IMAGE = 'http://localhost:4005/uploads';
 const IMIKINO_URL_IMAGE = 'https://imikino.rw/images';
 const BACKEND_URL_IMAGE = 'https://imikino-fullstack-448u5.ondigitalocean.app/uploads';
-const FRONTEND = 'https://imikino-frontend-bnd75.ondigitalocean.app';
+const FRONTEND = 'https://imikino.rw';
 
 // const LOCAL_URL = 'http://localhost:4000/api';
 // const HEROKU_URL = 'http://localhost:4000';
